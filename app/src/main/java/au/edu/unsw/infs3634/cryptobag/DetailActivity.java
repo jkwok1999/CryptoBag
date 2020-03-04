d319ff22a753b3fde5e9171d016f79a82aac47ef
jkwok1999 <jkwok1999@gmail.com>
2020-03-04 11:44:32 +1100

    3rd Commit - Semi-completed DetailActivity

--- app/src/main/java/au/edu/unsw/infs3634/cryptobag/DetailActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/cryptobag/DetailActivity.java
@@ -2,6 +2,7 @@ package au.edu.unsw.infs3634.cryptobag;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -12,14 +13,25 @@ public class DetailActivity extends AppCompatActivity {
     TextView change24h;
     TextView change7d;
     TextView marketcap;
-    TextView volumn;
+    TextView volume;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-    }
 
-    Coin newCoin = Coin.searchCoin();
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);//Gets the intent with the specified key
+
+        Coin newCoin = Coin.searchCoin(message);
+
+        value.setText(Double.toString(newCoin.getValue());
+        change1h.setText(Double.toString(newCoin.getChange1h());
+        change24h.setText(Double.toString(newCoin.getChange24h());
+        change7d.setText(Double.toString(newCoin.getChange7d());
+        marketcap.setText(Double.toString(newCoin.getMarketcap());
+        volume.setText(Double.toString(newCoin.getVolume());
+    }
 
 }
--- app/src/main/java/au/edu/unsw/infs3634/cryptobag/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/cryptobag/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Test1
     //private static final String TAG = "MainActivity";
-    //public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final String EXTRA_MESSAGE = "au.edu.unsw.infs3634.cryptobag.MESSAGE";
     private Button button;
 
     @Override
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the Send button */
     public void newActivity(View view) {
         Intent intent = new Intent(this, DetailActivity.class); //Creates new intent, linking the context('this' activity) to a class ('DetailActivity')
+        intent.putExtra(EXTRA_MESSAGE, "Bitcoin");
         startActivity(intent); //Starts the intent/instance of the DisplayMessageActivity that is specified by the intent
     }
 }
