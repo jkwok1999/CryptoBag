2dd7d167cd7f671de4f7ee0cd17c9825ede6f4cb
jkwok1999 <jkwok1999@gmail.com>
2020-03-04 11:55:30 +1100

    4th Commit - Added Google Search

--- app/src/main/java/au/edu/unsw/infs3634/cryptobag/DetailActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/cryptobag/DetailActivity.java
@@ -3,7 +3,10 @@ package au.edu.unsw.infs3634.cryptobag;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -14,6 +17,8 @@ public class DetailActivity extends AppCompatActivity {
     TextView change7d;
     TextView marketcap;
     TextView volume;
+    Button search;
+    Coin newCoin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +29,7 @@ public class DetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);//Gets the intent with the specified key
 
-        Coin newCoin = Coin.searchCoin(message);
+        newCoin = Coin.searchCoin(message);
 
         value.setText(Double.toString(newCoin.getValue());
         change1h.setText(Double.toString(newCoin.getChange1h());
@@ -34,4 +39,13 @@ public class DetailActivity extends AppCompatActivity {
         volume.setText(Double.toString(newCoin.getVolume());
     }
 
+    public void clickButton(View view) {
+        googleCoin(newCoin.getName());
+    }
+
+    public void googleCoin (String coinName) {
+        String url = "https://www.google.com/search?q=" + coinName;
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+    }
 }
